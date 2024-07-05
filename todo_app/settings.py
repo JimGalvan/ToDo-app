e6135775d6adb719e66502795f38ee88b4844390
asparrow-at-934390331891 <jimmy-galvan@live.com.mx>
2024-07-05 16:11:37 -0700

    add static folder path

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import json
 from pathlib import Path
-
+import os
 import boto3
 from botocore.exceptions import ClientError
 
@@ -156,6 +156,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
 
