d7cad5f4636caab55e03a4c09f7a2333e7173657
asparrow-at-934390331891 <jimmy-galvan@live.com.mx>
2024-07-06 18:35:45 -0700

    fix static folder

--- todo_app/settings/base.py
+++ todo_app/settings/base.py
@@ -80,7 +80,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
