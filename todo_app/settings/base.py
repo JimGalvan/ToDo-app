ec4b38b65dd7a9e41369787be20ffaff75249be1
asparrow-at-934390331891 <jimmy-galvan@live.com.mx>
2024-07-06 18:11:00 -0700

    update static folder

--- todo_app/settings/base.py
+++ todo_app/settings/base.py
@@ -81,9 +81,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'todo', 'static')
-
+# URL to use when referring to static files located in STATIC_ROOT.
 STATIC_URL = 'static/'
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional locations the staticfiles app will traverse if the FileSystemFinder finder is enabled.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
