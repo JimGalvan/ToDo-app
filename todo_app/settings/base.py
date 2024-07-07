90565451f130961dff97dfab7e0d635383c5dbed
asparrow-at-934390331891 <jimmy-galvan@live.com.mx>
2024-07-06 18:48:41 -0700

    fix static folder

--- todo_app/settings/base.py
+++ todo_app/settings/base.py
@@ -90,6 +90,8 @@ STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
 
+print(STATICFILES_DIRS)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
